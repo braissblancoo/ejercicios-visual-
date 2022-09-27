0c0d00f738580e8d49713a6abc17674ff498688c
braissblancoo <brais.blanco04@gmail.com>
2022-09-27 09:45:29 +0200

    bol2ejer8

---
+++ bol 2 ejerc 8/bol 2 ejerc 8/bol 2 ejer 13.vb
@@ -0,0 +1,19 @@
+﻿Module bol_2_ejer_13
+    Sub Main()
+        Dim catetoA, catetoB, hipotenusa As Double
+        Console.WriteLine("introduce el valor del cateto A: ")
+        catetoA = Convert.ToDouble(Console.ReadLine())
+
+        Console.WriteLine("introduce el valor del cateto B: ")
+        catetoB = Convert.ToDouble(Console.ReadLine())
+
+
+        hipotenusa = Math.Round(((catetoA ^ 2 + catetoB ^ 2) * Math.Sqrt(2)), 2)
+        Console.WriteLine("el valor de la hipotenusa es: " & hipotenusa)
+
+        Console.ReadLine()
+
+
+
+    End Sub
+End Module
